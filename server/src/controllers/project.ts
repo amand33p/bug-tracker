3ed6c0d085f490d4663c2d16a448f0cece5882fb
amand33p <amanmankoo96@gmail.com>
2021-01-02 17:44:00 +0530

    added create project route + relevant validator

--- server/src/controllers/project.ts
+++ server/src/controllers/project.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
 import { Project } from '../entity/Project';
 import { Member } from '../entity/Member';
+import { createProjectValidator } from '../utils/validators';
 
 export const createProject = async (req: Request, res: Response) => {
   const { name } = req.body;
-  const memberIds = req.body.members as string[];
+  const memberIds = [...req.body.members, req.user] as string[];
+  const { errors, valid } = createProjectValidator(name, memberIds);
+
+  if (!valid) {
+    return res.status(400).send({ message: Object.values(errors)[0] });
+  }
 
   const newProject = Project.create({
     name,
@@ -13,11 +19,10 @@ export const createProject = async (req: Request, res: Response) => {
   const savedProject = await newProject.save();
 
   const membersArray = memberIds.map((memberId) => ({
-    projectId: savedProject.id,
     memberId: memberId,
+    projectId: savedProject.id,
   }));
 
   await Member.insert(membersArray);
-
   res.status(201).json(savedProject);
 };
