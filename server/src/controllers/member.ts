ef4adcfdae3b165a036862d0d2ccf6ca9ab2519d
amand33p <amanmankoo96@gmail.com>
2021-01-05 14:41:15 +0530

    added route for leaving project

--- server/src/controllers/member.ts
+++ server/src/controllers/member.ts
@@ -3,39 +3,6 @@ import { Project } from '../entity/Project';
 import { Member } from '../entity/Member';
 import { projectMembersError } from '../utils/validators';
 
-export const removeProjectMember = async (req: Request, res: Response) => {
-  const { userId } = req.body;
-  const { projectId } = req.params;
-
-  const targetProject = await Project.findOne({
-    where: { id: projectId },
-    relations: ['members'],
-  });
-
-  if (!targetProject) {
-    return res.status(404).send({ message: 'Invalid project ID.' });
-  }
-
-  if (targetProject.createdById !== req.user) {
-    return res.status(401).send({ message: 'Access is denied.' });
-  }
-
-  if (targetProject.createdById === userId) {
-    return res
-      .status(400)
-      .send({ message: "Project creator can't be removed." });
-  }
-
-  if (!targetProject.members.map((m) => m.memberId).includes(userId)) {
-    return res.status(404).send({
-      message: "Member isn't part of the project or already removed.",
-    });
-  }
-
-  await Member.delete({ projectId, memberId: userId });
-  res.status(204).end();
-};
-
 export const addProjectMembers = async (req: Request, res: Response) => {
   const memberIds = req.body.members as string[];
   const { projectId } = req.params;
@@ -84,4 +51,62 @@ export const addProjectMembers = async (req: Request, res: Response) => {
     .getMany();
 
   res.status(201).json(updatedMembers);
+};
+
+export const removeProjectMember = async (req: Request, res: Response) => {
+  const { projectId, memberId } = req.params;
+
+  const targetProject = await Project.findOne({
+    where: { id: projectId },
+    relations: ['members'],
+  });
+
+  if (!targetProject) {
+    return res.status(404).send({ message: 'Invalid project ID.' });
+  }
+
+  if (targetProject.createdById !== req.user) {
+    return res.status(401).send({ message: 'Access is denied.' });
+  }
+
+  if (targetProject.createdById === memberId) {
+    return res
+      .status(400)
+      .send({ message: "Project creator can't be removed." });
+  }
+
+  if (!targetProject.members.map((m) => m.memberId).includes(memberId)) {
+    return res.status(404).send({
+      message: "Member isn't part of the project or already removed.",
+    });
+  }
+
+  await Member.delete({ projectId, memberId });
+  res.status(204).end();
+};
+
+export const leaveProjectAsMember = async (req: Request, res: Response) => {
+  const { projectId } = req.params;
+
+  const targetProject = await Project.findOne({
+    where: { id: projectId },
+    relations: ['members'],
+  });
+
+  if (!targetProject) {
+    return res.status(404).send({ message: 'Invalid project ID.' });
+  }
+
+  if (targetProject.createdById === req.user) {
+    return res.status(400).send({ message: "Project creator can't leave." });
+  }
+
+  if (!targetProject.members.map((m) => m.memberId).includes(req.user)) {
+    return res.status(404).send({
+      message: "You're not a member of the project.",
+    });
+  }
+
+  await Member.delete({ projectId, memberId: req.user });
+  res.status(204).end();
 };
--- server/src/routes/member.ts
+++ server/src/routes/member.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-import { addProjectMembers, removeProjectMember } from '../controllers/member';
+import {
+  addProjectMembers,
+  removeProjectMember,
+  leaveProjectAsMember,
+} from '../controllers/member';
 import middleware from '../middleware';
 
 const router = express.Router();
 const { auth } = middleware;
 
 router.post('/:projectId/members', auth, addProjectMembers);
-router.delete('/:projectId/members', auth, removeProjectMember);
+router.delete('/:projectId/members/:memberId', auth, removeProjectMember);
+router.delete('/:projectId/leave', auth, leaveProjectAsMember);
 
 export default router;
