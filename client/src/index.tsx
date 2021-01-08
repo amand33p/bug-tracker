584ec5aa1021474ed34cbe4f43b4575cf95e839b
amand33p <amanmankoo96@gmail.com>
2021-01-08 14:27:23 +0530

    added redux provider to capture the store; forgot it before

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
--- client/src/store.ts
+++ client/src/store.ts
@@ -6,4 +6,7 @@ const store = configureStore({
     user: userReducer,
   },
 });
+
 export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
