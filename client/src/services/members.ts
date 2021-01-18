6391548cde4d7fa0b48508b628fdd977a0dfd046
amand33p <amanmankoo96@gmail.com>
2021-01-18 22:59:52 +0530

    added apis for members to client

---
+++ client/src/services/members.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import backendUrl from '../backendUrl';
+import { token } from './auth';
+
+const baseUrl = `${backendUrl}/projects`;
+
+const setConfig = () => {
+  return {
+    headers: { 'x-auth-token': token },
+  };
+};
+
+const addMembers = async (projectId: string, members: string[]) => {
+  const response = await axios.post(
+    `${baseUrl}/${projectId}/members`,
+    { members },
+    setConfig()
+  );
+  return response.data;
+};
+
+const removeMember = async (projectId: string, memberId: string) => {
+  const response = await axios.delete(
+    `${baseUrl}/${projectId}/members/${memberId}`,
+    setConfig()
+  );
+  return response.data;
+};
+
+const leaveProjectMembership = async (projectId: string) => {
+  const response = await axios.delete(
+    `${baseUrl}/${projectId}/leave`,
+    setConfig()
+  );
+  return response.data;
+};
+
+const memberService = { addMembers, removeMember, leaveProjectMembership };
+
+export default memberService;
