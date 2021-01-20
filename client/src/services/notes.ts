7be0c886d1b7fc36f9ce2dcbdf40ea788196bb73
amand33p <amanmankoo96@gmail.com>
2021-01-20 22:23:22 +0530

    added notes API routes to client

---
+++ client/src/services/notes.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import backendUrl from '../backendUrl';
+import { setConfig } from './auth';
+
+const baseUrl = `${backendUrl}/projects`;
+
+const createNote = async (
+  projectId: string,
+  bugId: string,
+  noteBody: string
+) => {
+  const response = await axios.post(
+    `${baseUrl}/${projectId}/bugs/${bugId}/notes`,
+    { body: noteBody },
+    setConfig()
+  );
+  return response.data;
+};
+
+const editNote = async (
+  projectId: string,
+  noteId: string,
+  noteBody: string
+) => {
+  const response = await axios.put(
+    `${baseUrl}/${projectId}/notes/${noteId}`,
+    { body: noteBody },
+    setConfig()
+  );
+  return response.data;
+};
+
+const deleteNote = async (projectId: string, noteId: string) => {
+  const response = await axios.delete(
+    `${baseUrl}/${projectId}/notes/${noteId}`,
+    setConfig()
+  );
+  return response.data;
+};
+
+const noteService = {
+  createNote,
+  editNote,
+  deleteNote,
+};
+
+export default noteService;
